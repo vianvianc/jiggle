d41446f7cdef12cadbfe487db1f59fa9d23486b6
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-07-14 14:29:23 -0600

    edited js events

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 document.getElementById("button1").addEventListener("click", function() {
 
-    document.getElementById("box").style.height = "250px";
+    document.getElementById("box").style.height = "400px";
+    document.getElementById("box").style.width = "400px";
 });
 
 document.getElementById("button2").addEventListener("click", function(){
@@ -14,7 +15,9 @@ document.getElementById("button4").addEventListener("click", function(){
 
     document.getElementById("box").style.height = "150px";
     document.getElementById("box").style.backgroundColor = "orange";
-
+    document.getElementById("box").innerText = " ";
+    document.getElementById("box").style.backgroundImage = "";
+    document.getElementById("box").style.width = "150px";
 });
 
 
@@ -36,3 +39,13 @@ function fadeIn(){
     document.getElementById("box").style.opacity = 1;
     document.getElementById("button3").innerHTML = "Fade Out";
 }
+
+document.getElementById("button5").addEventListener("click", function() {
+
+    document.getElementById("box").innerText = "Hello";
+});
+
+document.getElementById("button6").addEventListener("click", function() {
+
+    document.getElementById("box").style.backgroundImage = "url('https://www.francerocks.com/wp-content/themes/replay/framework/extensions/timthumb/timthumb.php?src=https%3A%2F%2Fwww.francerocks.com%2Fwp-content%2Fuploads%2F2019%2F05%2Fyuksek-polo-pan-Cadenza.jpeg&w=610')";
+});
