dd1a95cdf3c83cd411f6e36f425c60533ce948f1
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-07-14 13:07:27 -0600

    added some js events

---
+++ javascript.js
@@ -0,0 +1,21 @@
+document.getElementById("button1").addEventListener("click", function() {
+
+    document.getElementById("box").style.height = "250px";
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+    document.getElementById("box").style.backgroundColor = "blue";
+
+});
+
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+
+});
+
+
+
